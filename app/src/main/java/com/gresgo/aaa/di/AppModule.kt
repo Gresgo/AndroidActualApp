857f6bc15a2c7c9dd60e4d265e301deff97b9ba9
Gresgo <amagresgo@gmail.com>
2022-04-06 14:33:41 +0500

    Provide app router with hilt

---
+++ app/src/main/java/com/gresgo/aaa/di/AppModule.kt
@@ -0,0 +1,18 @@
+package com.gresgo.aaa.di
+
+import android.app.Application
+import com.gresgo.aaa.App
+import com.gresgo.core_navigation.Router
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    fun provideRouter(app: Application): Router = (app as App).router
+
+}
