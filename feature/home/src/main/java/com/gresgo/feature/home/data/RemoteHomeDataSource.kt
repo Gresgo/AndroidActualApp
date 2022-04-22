9d499067b66ae826dc291b89bce738b54a515494
Gresgo <amagresgo@gmail.com>
2022-04-22 12:54:25 +0500

    Add home remote base implementation

--- feature/home/src/main/java/com/gresgo/feature/home/data/RemoteHomeDataSource.kt
+++ feature/home/src/main/java/com/gresgo/feature/home/data/RemoteHomeDataSource.kt
@@ -4,4 +4,10 @@ import com.gresgo.feature.home.data.model.GitRepoData
 
 interface RemoteHomeDataSource {
     suspend fun fetch(): List<GitRepoData>
+
+    class Base: RemoteHomeDataSource {
+        override suspend fun fetch(): List<GitRepoData> {
+            return emptyList()
+        }
+    }
 }
