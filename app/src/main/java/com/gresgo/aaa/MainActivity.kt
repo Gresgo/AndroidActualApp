f338689e9337e22412d0b3477a8497b5b8b5542e
Gresgo <amagresgo@gmail.com>
2022-04-22 13:24:33 +0500

    Modify app theme

--- app/src/main/java/com/gresgo/aaa/MainActivity.kt
+++ app/src/main/java/com/gresgo/aaa/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
@@ -28,11 +29,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MainTheme(darkTheme = true) {
-                Column(modifier = Modifier.fillMaxSize()) {
-                    TopAppBar(title = {
-                        Text(text = "Compose App")
-                    })
-                    NavigationHost()
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        TopAppBar(title = {
+                            Text(text = "Compose App")
+                        })
+                        NavigationHost()
+                    }
                 }
             }
         }
